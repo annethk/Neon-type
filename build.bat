ffceff1dc03a3b8d9527d363088a7b446e9e63a4
Keicy <keicykrl@gmail.com>
2026-07-02 02:51:44 -0300

    Build.bat problema corrigido

--- build.bat
+++ build.bat
@@ -1,10 +1,15 @@
 @echo off
+:: Adiciona o caminho do compilador ao PATH temporariamente
+set PATH=C:\msys64\ucrt64\bin;%PATH%
+
 echo Compilando o NeonType...
-gcc -I include src/*.c -o neontype.exe -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc -Iinclude src/*.c -o neontype.exe -lraylib -lopengl32 -lgdi32 -lwinmm -lws2_32
+
 if %errorlevel% equ 0 (
     echo Compilacao bem-sucedida!
     neontype.exe
 ) else (
     echo Erro na compilacao!
+    echo Verifique se o MSYS2 esta instalado em C:\msys64
 )
 pause
