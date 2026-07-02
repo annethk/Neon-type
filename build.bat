227df65f6083dc088469329944673abfe86c7b90
Keicy <keicykrl@gmail.com>
2026-07-02 05:15:16 -0300

    feat: adiciona icone de alta qualidade ao executavel

--- build.bat
+++ build.bat
@@ -1,15 +1,17 @@
 @echo off
-:: Adiciona o caminho do compilador ao PATH temporariamente
 set PATH=C:\msys64\ucrt64\bin;%PATH%
 
-echo Compilando o NeonType...
-gcc -Iinclude src/*.c -o neontype.exe -lraylib -lopengl32 -lgdi32 -lwinmm -lws2_32
+echo Preparando o icone...
+windres recursos.rc -O coff -o recursos.res
+
+echo Compilando o NeonType com icone...
+gcc -Iinclude src/*.c recursos.res -o neontype.exe -lraylib -lopengl32 -lgdi32 -lwinmm -lws2_32
 
 if %errorlevel% equ 0 (
     echo Compilacao bem-sucedida!
+    del recursos.res
     neontype.exe
 ) else (
     echo Erro na compilacao!
-    echo Verifique se o MSYS2 esta instalado em C:\msys64
+    pause
 )
-pause
